a66493a7db40743fdc3bfcafdbdc7c90f292e29a
Chai Sarfati <chaisarfati@gmail.com>
2019-03-13 22:47:04 +0100

    grey improved, nearest doesn't work

--- SC_Unimplemented/src/edu/cg/ImageProcessor.java
+++ SC_Unimplemented/src/edu/cg/ImageProcessor.java
@@ -101,9 +101,9 @@ public class ImageProcessor extends FunctioalForEachLoops {
 
         forEach((y, x) -> {
             Color c = new Color(workingImage.getRGB(x, y));
-            int red = c.getRed();
-            int green = c.getGreen();
-            int blue = c.getBlue();
+            int red = r * c.getRed() / max;
+            int green = g * c.getGreen() / max;
+            int blue = b * c.getBlue() / max;
             int avg = (red+green+blue)/3;
             Color newColor = new Color(avg,avg,avg);
             ans.setRGB(x, y, newColor.getRGB());
@@ -115,6 +115,39 @@ public class ImageProcessor extends FunctioalForEachLoops {
 
 	public BufferedImage nearestNeighbor() {
 		// TODO: Implement this method, remove the exception.
-		throw new UnimplementedMethodException("nearestNeighbor");
+        logger.log("Preparing for Nearest neighbor changing...");
+
+        BufferedImage ans = newEmptyImage(outWidth, outHeight);
+
+        int factHeight = outHeight / inHeight,
+                factWidth = outWidth / inWidth;
+
+        int currPixHeight = 0, currPixWidth = 0;
+        for (int i = 0; i < outWidth; i++) {
+            if(i != 0) currPixWidth += factWidth;
+            for (int j = 0; j < outHeight; j++) {
+                if(j != 0) currPixHeight += factHeight;
+                Color c = new Color(workingImage.getRGB(currPixWidth, currPixHeight));
+                int red = c.getRed();
+                int green = c.getGreen();
+                int blue = c.getBlue();
+                Color newColor = new Color(red, green, blue);
+                ans.setRGB(i, j, newColor.getRGB());
+            }
+            currPixHeight = 0;
+        }
+        /*
+        forEach((y,x) -> {
+            Color c = new Color(workingImage.getRGB(x, y));
+            int red = c.getRed();
+            int green = c.getGreen();
+            int blue = c.getBlue();
+            Color newColor = new Color(red, green, blue);
+            ans.setRGB(x, y, newColor.getRGB());
+        });
+        */
+
+        logger.log("Changing greyscale done!");
+        return ans;
 	}
 }
